cc669631563fee57c74877d0d98b29ad51b0c63e
Luis Diaz <luisdiazugena@gmail.com>
2015-06-19 16:49:55 +0200

    Initial FW of lightTable

---
+++ LightTableFW/LightTableFW.ino
@@ -0,0 +1,49 @@
+
+
+/*
+
+Attiny85 based light table --
+
+Firmware
+
+*/
+
+int trimmer = A1, out = 0;
+float trimValue = 0, lastTrimValue = 0;
+int threeshold = 200;
+
+void setup() {
+
+	pinMode(trimmer, INPUT);
+	pinMode(out, OUTPUT);
+
+	digitalWrite(out, LOW);
+	test();
+}
+void loop() {
+
+	trimValue = analogRead(trimmer);
+	if (trimValue < (0 + threeshold)) {
+		digitalWrite(out, LOW);
+	} else if (trimValue > (1023 - threeshold)) {
+		digitalWrite(out, HIGH);
+	} else if (trimValue != lastTrimValue) {
+		analogWrite(out, map(trimValue, 0, 1023, 0, 255));
+	}
+
+
+
+	lastTrimValue = trimValue;
+}
+
+void test() {
+	int j;
+	for (j = 0; j <= 255; j++) {
+		analogWrite(out, j);
+		delay(10);
+	}
+	for (j = 255; j >= 0; j--) {
+		analogWrite(out, j);
+		delay(10);
+	}
+}
